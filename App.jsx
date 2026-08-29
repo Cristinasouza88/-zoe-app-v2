3342f8fcdf73fbba76ceff17bf0117feb398c0b5
Cristinasouza88 <94078152+Cristinasouza88@users.noreply.github.com>
2026-08-29 10:47:43 -0300

    Ativa ditado global e proteção de foco

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import './niil-brand.css';
 import './niil-flat-theme.css';
 import './niil-runtime.js';
+import './niil-input-runtime.js';
 import NIILAppCore from './NiilAppCore.jsx';
 
 export default NIILAppCore;
